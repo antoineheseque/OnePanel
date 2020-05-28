67a21cbf2699a726430900d03a2bea5b9e431425
sheila <marthe-sheila.gnitedemg@student.yncrea.fr>
2020-05-28 16:43:06 +0200

    maj je teste un truc

--- src/pages/Profile/EditProfileForm.vue
+++ src/pages/Profile/EditProfileForm.vue
@@ -3,13 +3,13 @@
         <h5 slot="header" class="title">Mon Profil</h5>
         <div class="row">
 
-            <div class="col-md-3 px-md-1">
+            <div class="col-md-3 px-md-3">
                 <base-input label="Nom d'utilisateur"
                             placeholder="Username"
                             v-model="profile.username">
                 </base-input>
             </div>
-            <div class="col-md-4 pl-md-1">
+            <div class="col-md-5 pl-md-1">
                 <base-input label="Adresse Mail"
                             type="email"
                             placeholder="mike@email.com"
@@ -18,13 +18,13 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 pr-md-1">
+            <div class="col-md-3 pr-md-3">
                 <base-input label="Nom"
                             v-model="profile.firstName"
                             placeholder="First Name">
                 </base-input>
             </div>
-            <div class="col-md-6 pl-md-1">
+            <div class="col-md-3 pl-md-1">
                 <base-input label="Prénom"
                             v-model="profile.lastName"
                             placeholder="Last Name">
@@ -32,7 +32,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-6">
                 <base-input label="Addresse"
                             v-model="profile.address"
                             placeholder="Home Address">
@@ -40,31 +40,20 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4 pr-md-1">
+            <div class="col-md-3 pr-md-3">
                 <base-input label="City"
                             v-model="profile.city"
                             placeholder="City">
                 </base-input>
             </div>
-            <div class="col-md-4 px-md-1">
+            <div class="col-md-3 px-md-1">
                 <base-input label="Country"
                             v-model="profile.country"
                             placeholder="Country">
                 </base-input>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-8">
-                <base-input>
-                    <label>About Me</label>
-                    <textarea rows="4" cols="80"
-                              class="form-control"
-                              placeholder="Here can be your description"
-                              v-model="profile.description">
-              </textarea>
-                </base-input>
-            </div>
-        </div>
+
         <base-button slot="footer" type="primary"  v-on:click="onClickEditProfile" fill>Save</base-button>
     </card>
 </template>
