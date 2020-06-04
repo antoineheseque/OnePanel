151a3ac7f819ea6370057b56e478c6e043479352
sheila <marthe-sheila.gnitedemg@student.yncrea.fr>
2020-06-04 12:02:17 +0200

    widgets

--- src/pages/AddWidget.vue
+++ src/pages/AddWidget.vue
@@ -1,17 +1,34 @@
 <template>
+  <div>
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-2">
       <add-widget-list :widgetList="widgetList">
       </add-widget-list>
     </div>
 
-    <div class="col-md-4">
-      <div class="column">
-        <widget-card :widgetPreview="widgetPreview"></widget-card>
-        <base-button slot="footer" type="primary" fill>Save</base-button>
+      <div class="col-lg-6 col-md-1">
+        <card type="tasks">
+
+            <h1 v-bind:class="display-3">Widgets</h1>
+            <div class="row">
+          <div class="col-md-3" v-for="produit in produits">
+            <p class="jumbotorn">{{produit}} <br>
+              <base-dropdown menu-on-right=""
+                             tag="div"
+                             title-classes="btn btn-link btn-icon"
+                             aria-label="Settings menu">
+                <i slot="title" class="tim-icons icon-settings-gear-63"></i>
+                <a class="dropdown-item" href="http://localhost:8081/dashboard">Ajouter</a>
+              </base-dropdown>
+            </p>
+          </div>
+            </div>
+        </card>
       </div>
-    </div>
   </div>
+
+  </div>
+
 </template>
 <script>
   import WidgetList from './AddWidget/WidgetList';
@@ -24,12 +41,15 @@
     },
     data() {
       return {
-        widgetList: {
-
-        },
-        widgetPreview: {
-
-        }
+        widgetList: {},
+        produits: ["Horoscope", "Météo","Transports","Bitcoin","Pokémon","Suivi de colis","Gif du jour","Day streak","classement de foot","Horloge Mondiale","citation du jour"],
+        widgetPreview: {}
+      }
+    },
+    props: ['nom'],
+    methods: {
+      sélectionner: function (produit) {
+        this.produits.push(produit);
       }
     }
   }
