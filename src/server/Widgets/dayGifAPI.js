25227278501f36d7c35b3bc743cb8af9c0c2104c
Antoine Heseque <hesequeantoine@gmail.com>
2020-06-09 12:43:17 +0200

    Gif update fonctionnel

--- src/server/Widgets/dayGifAPI.js
+++ src/server/Widgets/dayGifAPI.js
@@ -12,10 +12,6 @@ router.post("/getImage", (req, res) => {
     //fonction qui demande l'url d'une image dans la BDD
 
     sql.request(`SELECT Lien from \`Gif_du_jour\` WHERE id ='1' LIMIT 1`).then(function (result){
-
-        console.log("\nselection_url_image_bdd=")
-        console.log(result[0].Lien)
-
         res.json({"image":result[0].Lien})
     })
 });
@@ -23,32 +19,18 @@ router.post("/getImage", (req, res) => {
 ///////////////////////////////////////////////////
 // SERVER -> API
 ///////////////////////////////////////////////////
+// POUR CREER L'IMAGE: /api/widget/daygif/createImage
 
-router.post("/createImage", (req, res) => {
+router.get("/createImage", (req, res) => {
 
-    fetch('https://api.tenor.com/v1/trending?key=EW6KT86NL2K6&limit=1', {
-        method: 'GET'
-    }).then(function (res) {
-        return res.json()
-    }).then(function (data) {
-        const image_API = data[0]["media"][0]["gif"]["url"]; // CONTIENT L'URL DE L'IMAGE
-        console.log("\nimage_API=")
-        console.log(image_API)
+    axios.get("https://api.tenor.com/v1/trending?key=EW6KT86NL2K6&limit=1").then(function(r){
+        const image_API = r.data.results[0]["media"][0]["gif"].url; // CONTIENT L'URL DE L'IMAGE
 
-        //SI LA TABLE EST VIDE, ON AJOUTE UNE PREMIERE IMAGE SINON ON UPDATE JUSTE LA TABLE
-        sql.request('SELECT * FROM \`Gif_du_jour\` WHERE Lien IS NULL').then(function(result){
-            console.log("\nresult_table_null?=")
-            console.log(result)
-
-            if (result === null || result === undefined) {
-                //fonction qui envoie l'url de l'image reçu de l'API dans la BDD LA PREMIERE FOIS
-                sql.request(`INSERT INTO \`Gif_du_jour\`(Lien) VALUE('${image_API}')`);
-            } else {
-                //fonction qui update la bdd à chaque actualisation
-                sql.request(`UPDATE \`Gif_du_jour\` SET (Lien) VALUE('${image_API}') WHERE id=1 LIMIT 1`);
-            }
-        })
-        console.log("Chargement API Gif")
+        sql.request(`UPDATE \`Gif_du_jour\` SET Lien='${image_API}' WHERE id=1`).then(function (result){
+            res.json({"success":"true"})
+        });
+    }).catch(function(result){
+        console.log(result)
     })
 });
 
