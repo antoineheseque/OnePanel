db4828627bc0c333c6b4e44d3de2fbf301f12f16
Antoine Heseque <hesequeantoine@gmail.com>
2020-06-12 12:10:39 +0200

    AddWidget.vue WidgetList.vue

--- src/pages/AddWidget.vue
+++ src/pages/AddWidget.vue
@@ -1,77 +1,75 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-md-8">
-        <widget-list :widgetList="widgetList"></widget-list>
-      </div>
+    <div>
+        <div class="row">
+            <widget-list :widgetList="widgetList"></widget-list>
+        </div>
     </div>
-  </div>
 
 </template>
 <script>
-  import widgetsList from '../widgetsList.json';
-  import WidgetList from './AddWidget/WidgetList';
+    import widgetsList from '../widgetsList.json';
+    import WidgetList from './AddWidget/WidgetList';
 
-  export default {
+    export default {
 
-    components: {
-      WidgetList,
-    },
-    data() {
-      return {
-        widgetList:widgetsList.widgets
-      }
-    },
-    methods: {
-        /*onClickSupprime: function () {
-            this.isLogging = true
-
-            //this.notify('info', 'Connexion en cours.')
-            User.login(this.login).then((result) => {
-                console.log(result)
-
-                if(result.logged === true){ // Si l'utilisateur à pu être connecté
-                    this.notify('info', `Widget ajouté avec succès.`)
-                    router.push('dashboard')
-                }
-                else{
-                    this.login.password = ""
-                    this.notify('danger', result.reason)
-                }
-                this.isLogging = false
-            });
+        components: {
+            WidgetList,
         },
-        notify: function(info,message){
-            this.$notify({
-                component: NotificationTemplate,
-                icon: "tim-icons icon-bell-55",
-                horizontalAlign: "right",
-                verticalAlign: "top",
-                type: info,
-                timeout: 2000,
-                message: message
-            })
+        data() {
+            return {
+                widgetList:widgetsList.widgets
+            }
         },
-        onClickAjout: function () {
-            this.isLogging = true
+        methods: {
+            /*onClickSupprime: function () {
+                this.isLogging = true
 
-            //this.notify('info', 'Connexion en cours.')
-            User.login(this.login).then((result) => {
-                console.log(result)
+                //this.notify('info', 'Connexion en cours.')
+                User.login(this.login).then((result) => {
+                    console.log(result)
 
-                if(result.logged === true){ // Si l'utilisateur à pu être connecté
-                    this.notify('info', `Widget ajouté avec succès.`)
-                    router.push('dashboard')
-                }
-                else{
-                    this.login.password = ""
-                    this.notify('danger', result.reason)
-                }
-                this.isLogging = false
-            });
-        }*/
+                    if(result.logged === true){ // Si l'utilisateur à pu être connecté
+                        this.notify('info', `Widget ajouté avec succès.`)
+                        router.push('dashboard')
+                    }
+                    else{
+                        this.login.password = ""
+                        this.notify('danger', result.reason)
+                    }
+                    this.isLogging = false
+                });
+            },
+            notify: function(info,message){
+                this.$notify({
+                    component: NotificationTemplate,
+                    icon: "tim-icons icon-bell-55",
+                    horizontalAlign: "right",
+                    verticalAlign: "top",
+                    type: info,
+                    timeout: 2000,
+                    message: message
+                })
+            },
+            onClickAjout: function () {
+                this.isLogging = true
+
+                //this.notify('info', 'Connexion en cours.')
+                User.login(this.login).then((result) => {
+                    console.log(result)
+
+                    if(result.logged === true){ // Si l'utilisateur à pu être connecté
+                        this.notify('info', `Widget ajouté avec succès.`)
+                        router.push('dashboard')
+                    }
+                    else{
+                        this.login.password = ""
+                        this.notify('danger', result.reason)
+                    }
+                    this.isLogging = false
+                });
+            }*/
+        }
     }
-  }
 </script>
 <style>
 </style>
--- src/pages/AddWidget/WidgetList.vue
+++ src/pages/AddWidget/WidgetList.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
-  <div class="row">
-    <div class="col-md-6" v-for="(widget,idxx) in widgetList" :key="idxx">
-  <card style="width: 20rem;" >
-    <img slot="image" class="card-img-top" src="widget.image">
-    <h4 class="card-title">{{widget.title}}</h4>
-    <p class="card-text">{{widget.description}}</p>
-    <a href="#" class="btn btn-success">Ajouter</a>
-  </card>
-
-</div>
-  </div>
-  </div>
+    <div>
+        <div class="row">
+            <div class="col-md-3" v-for="(widget,idxx) in widgetList" :key="idxx">
+                <card>
+                    <img slot="image" class="card-img-top" :src="widget.image">
+                    <h4 class="card-title">{{widget.title}}</h4>
+                    <p class="card-text">{{widget.description}}</p>
+                    <a href="#" class="btn btn-success">Ajouter</a>
+                </card>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 
-  export default {
+    export default {
 
-    name: "add-widget-list",
-    props: {
-      widgetList:""
+        name: "add-widget-list",
+        props: {
+            widgetList:""
+        }
+
     }
-
-  }
 </script>
