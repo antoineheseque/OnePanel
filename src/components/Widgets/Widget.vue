bd7de972449c6812e23cc43dfa75b8deed4c4049
Antoine Heseque <hesequeantoine@gmail.com>
2020-06-15 10:41:12 +0200

    Fix widgets text https://trello.com/c/SVrzl15L/37-lorsque-lon-supprime-un-widget-le-nom-sp%C3%A9cial-par-ex-pr-meteo-ou-horoscope-se-d%C3%A9cale-a-lautre-widget

--- src/components/Widgets/Widget.vue
+++ src/components/Widgets/Widget.vue
@@ -3,8 +3,8 @@
         <div class="card" :class="[type && `card-${type}`]">
             <div class="card-header">
                 <slot name="title" class="header">
-                    <h5 class="title d-inline" v-if="weatherInformations">{{widget.title + " de " + weatherInformations}}</h5>
-                    <h5 class="title d-inline" v-else-if="horoscopeInformations">{{widget.title + " " + horoscopeInformations}}</h5>
+                    <h5 class="title d-inline" v-if="weatherInformations && widget.id==='meteo'">{{widget.title + " de " + weatherInformations}}</h5>
+                    <h5 class="title d-inline" v-else-if="horoscopeInformations && widget.id==='horoscope'">{{widget.title + " " + horoscopeInformations}}</h5>
                     <h5 class="title d-inline" v-else>{{widget.title}}</h5>
 
                     <base-dropdown menu-on-right=""
